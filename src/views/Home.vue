df6fe0bcfc64c28c0a1717cf932139f83709f7d8
Sun1ive <sunliveua@gmail.com>
2018-06-23 20:42:03 +0300

    takeitboy

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -76,7 +76,7 @@
 import Vue from 'vue';
 import * as T from '@/Types/index.d.ts';
 import LocalStorage from '@/utils';
-import Snackbar from '@/mixins/snackbar';
+import Snackbar from '../mixins/snackbar';
 
 export default Vue.extend({
   mixins: [Snackbar],
